f9bf6b0336bbb87fcbce8acdd007b3285a0b14c1
Jonathan Ramiro Elías Vásquez <99297546+JonathanREV2003@users.noreply.github.com>
2024-07-31 17:11:47 -0600

    Ultima version integracion de ValidNumber

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,32 @@
 package org.example;
 
 import org.example.calculator.Calculator;
+import org.example.calculator.InputValidator;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
-        System.out.println("Suma: " + calculator.addition(5, 7));
-        System.out.println("Resta: " + calculator.subtract(5, 3));
-        System.out.println("Multiplicacion: " + calculator.multiply(7, 7));
-        System.out.println("Divicion: " + calculator.divide(9, 3));
+
+        System.out.print("Escriba el primer numero: ");
+        String input1 = scanner.nextLine();
+
+        System.out.print("Escriba el segundo numero: ");
+        String input2 = scanner.nextLine();
+
+        if (InputValidator.isValidNumber(input1) && InputValidator.isValidNumber(input2)) {
+
+            int num1 = Integer.parseInt(input1);
+            int num2 = Integer.parseInt(input2);
+            System.out.println("Suma: " + calculator.addition(num1, num2));
+            System.out.println("Resta: " + calculator.subtract(num1, num2));
+            System.out.println("Multiplicacion: " + calculator.multiply(num1, num2));
+            System.out.println("Divicion: " + calculator.divide(num1, num2));
+
+        }else{
+            System.out.println("Entrada incorrecta de datos. Por favor escriba un numero correcto");
+        }
     }
 }
